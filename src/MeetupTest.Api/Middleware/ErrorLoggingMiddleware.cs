1597e8173e09270f4137aa2abaa8503409b648c2
Sean Adamson <sean.adamson@4com.co.uk>
2017-07-03 18:07:33 +0100

    Added error logging middleware

---
+++ src/MeetupTest.Api/Middleware/ErrorLoggingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace MeetupTest.Api.Middleware
+{
+    public class ErrorLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private ILogger<ErrorLoggingMiddleware> _logger;
+
+        public ErrorLoggingMiddleware(RequestDelegate next, ILogger<ErrorLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(0, e, e.Message);
+            }
+        }
+    }
+}
---
+++ tests/MeetupTest.Api.Tests/UnitTests/Middleware/ErrorLoggingMiddlewareTests.cs
@@ -0,0 +1,29 @@
+﻿using MeetupTest.Api.Middleware;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MeetupTest.Api.Tests.UnitTests.Middleware
+{
+    public class ErrorLoggingMiddlewareTests
+    {
+        private ErrorLoggingMiddleware _middleware;
+        private Mock<ILogger<ErrorLoggingMiddleware>> _logger;
+
+        public ErrorLoggingMiddlewareTests()
+        {
+            _logger = new Mock<ILogger<ErrorLoggingMiddleware>>();
+            _middleware = new ErrorLoggingMiddleware(new RequestDelegate((context) => throw new DivideByZeroException()), _logger.Object);
+        }
+
+        [Fact]
+        public async Task Invoke_LogsWhenExceptionIsThrown()
+        {
+            await _middleware.Invoke(new DefaultHttpContext());
+            _logger.Verify(o => o.Log(LogLevel.Error, It.IsAny<EventId>(), It.IsAny<object>(), It.IsAny<DivideByZeroException>(), It.IsAny<Func<object, Exception, string>>()));
+        }
+    }
+}
