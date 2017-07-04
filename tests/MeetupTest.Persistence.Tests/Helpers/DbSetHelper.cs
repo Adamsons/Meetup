7552040b191603f1b275cd282e85f4bcbe3d96fb
Sean Adamson <git@seanadamson.com>
2017-07-04 18:37:15 +0100

    Added test helpers for EF core context

---
+++ tests/MeetupTest.Persistence.Tests/Helpers/DbSetHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace MeetupTest.Persistence.Tests.Helpers
+{
+    public static class DbSetHelper
+    {
+        public static Mock<DbSet<T>> MockDbSet<T>(IEnumerable<T> list) where T : class, new()
+        {
+            IQueryable<T> queryableList = list.AsQueryable();
+            Mock<DbSet<T>> dbSetMock = new Mock<DbSet<T>>();
+            dbSetMock.As<IQueryable<T>>().Setup(x => x.Provider).Returns(queryableList.Provider);
+            dbSetMock.As<IQueryable<T>>().Setup(x => x.Expression).Returns(queryableList.Expression);
+            dbSetMock.As<IQueryable<T>>().Setup(x => x.ElementType).Returns(queryableList.ElementType);
+            dbSetMock.As<IQueryable<T>>().Setup(x => x.GetEnumerator()).Returns(queryableList.GetEnumerator());
+            dbSetMock.As<IAsyncEnumerable<T>>().Setup(m => m.GetEnumerator()).Returns(new TestAsyncEnumerator<T>(queryableList.GetEnumerator()));
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.Provider).Returns(new TestAsyncQueryProvider<T>(queryableList.Provider));
+
+            return dbSetMock;
+        }
+    }
+}
